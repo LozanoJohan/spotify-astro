170ab861adbd14febb302f29ecdd35b12ce95558
LozanoJohan <lozanojohan321@gmail.com>
2023-10-12 14:40:13 -0500

    second commit

---
+++ src/components/Player.jsx
@@ -0,0 +1,59 @@
+import { useState, useRef, useEffect } from "react"
+
+const Pause = () => (
+    <svg role="img" className="h-8 w-8" aria-hidden="true" viewBox="0 0 16 16"
+    ><path
+        d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"
+    ></path></svg>
+
+)
+
+const Play = () => (
+    <svg viewBox="0 0 24 24" className="h-8 w-8" fill="currentColor"
+    ><path fill="currentColor" d="M8 5.14v14l11-7-11-7z"></path></svg>
+)
+
+export function Player() {
+
+    const [isPlaying, setIsPlaying] = useState(false)
+    const [currentSong, setCurrentSong] = useState(null)
+
+    const audioRef = useRef()
+
+    useEffect(() => {
+        audioRef.current.src = `/music/1/01.mp3`
+
+    }, [])
+
+    const handleClick = () => {
+        if (isPlaying) {
+            audioRef.current.pause()
+        } else {
+            audioRef.current.play()
+        }
+
+        setIsPlaying(!isPlaying)
+        audioRef.current.volume = 1
+    }
+
+    return (
+        <div className="flex justify-between w-full px-4 z-50">
+            <div>
+                current song
+            </div>
+            <div className="grid place-content-center gap-4 flex-1">
+                <div className="flex justify-center">
+                    <button className="bg-white rounded-full p-2"
+                        onClick={handleClick}>
+                        {isPlaying ? <Pause /> : <Play />}
+                    </button>
+                </div>
+            </div>
+            <div className="grid place-content-center">
+                volumen
+            </div>
+
+            <audio ref={audioRef}></audio>
+        </div>
+    )
+}
