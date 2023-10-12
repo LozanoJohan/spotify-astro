94e50f77dee7ae22b405d9819da419343e0f723f
LozanoJohan <lozanojohan321@gmail.com>
2023-10-12 15:41:43 -0500

    deploy to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
 import react from "@astrojs/react";
+import vercel from '@astrojs/vercel/serverless';
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), react()],
-  output: 'server'
+  output: 'server',
+  adapter: vercel(),
+
 });
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,7 @@
 import { useState, useRef, useEffect } from "react"
+import { usePlayerStore } from '../store/playerStore'
 
-const Pause = () => (
+export const Pause = () => (
     <svg role="img" className="h-8 w-8" aria-hidden="true" viewBox="0 0 16 16"
     ><path
         d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"
@@ -8,14 +9,14 @@ const Pause = () => (
 
 )
 
-const Play = () => (
+export const Play = () => (
     <svg viewBox="0 0 24 24" className="h-8 w-8" fill="currentColor"
     ><path fill="currentColor" d="M8 5.14v14l11-7-11-7z"></path></svg>
 )
 
 export function Player() {
 
-    const [isPlaying, setIsPlaying] = useState(false)
+    const {isPlaying, setIsPlaying} = usePlayerStore(state => state)
     const [currentSong, setCurrentSong] = useState(null)
 
     const audioRef = useRef()
