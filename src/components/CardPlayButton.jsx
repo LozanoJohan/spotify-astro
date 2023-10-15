8f9d76e93e8e1fd3942b2f78f4f5bd74754a4d82
LozanoJohan <lozanojohan321@gmail.com>
2023-10-15 18:41:58 -0500

    volume

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -1,4 +1,4 @@
-import {Pause, Play} from "./Player"
+import { Pause, Play } from "./Player"
 import { usePlayerStore } from '../store/playerStore'
 
 export const CardPlayButton = ({ id }) => {
@@ -6,17 +6,24 @@ export const CardPlayButton = ({ id }) => {
     const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic } = usePlayerStore(state => state)
 
     const handleClick = () => {
-        setCurrentMusic({
-            playlist: {
-                id
-            }
-        })
-        setIsPlaying(!isPlaying)
+        if (isPlayingPlaylist) {
+            setIsPlaying(!isPlaying)
+            return
+        }
+
+        fetch(`/api/get-info-playlist.json?id=${id}`)
+            .then(res => res.json())
+            .then(data => {
+                const { songs, playlist } = data
+                setIsPlaying(true)
+                setCurrentMusic({ songs, playlist, song: songs[0] })
+
+            })
     }
 
     const isPlayingPlaylist = isPlaying && currentMusic?.playlist.id === id
 
     return <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-2">
-        {isPlayingPlaylist ? <Pause/> : <Play/>}
+        {isPlayingPlaylist ? <Pause /> : <Play />}
     </button>
 }
--- src/pages/api/get-info-playlist.json.js
+++ src/pages/api/get-info-playlist.json.js
@@ -1,2 +1,13 @@
-export async function GET({ params, reques}) {
+import { allPlaylists, songs as allSongs } from "../../lib/data";
+
+export async function GET({ params, request }) {
+    // Get the id from the url
+    const { url } = request
+    const urlObject = new URL(url)
+    const id = urlObject.searchParams.get('id')
+
+    const playlist = allPlaylists.find(playlist => playlist.id === id)
+    const songs = allSongs.filter(song => song.albumId === playlist?.albumId)
+
+    return new Response(JSON.stringify({playlist, songs}))
 }
